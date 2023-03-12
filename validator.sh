091c110058ab68c47c3f515d79a306ca3a97e3e9
A-Aleksey-A <126988346+A-Aleksey-A@users.noreply.github.com>
2023-03-12 21:48:41 +0200

    Update validator.sh

--- validator.sh
+++ validator.sh
@@ -2,17 +2,17 @@
 
 # Check the balance of the account
 balance=$(pcli view balance | grep penumbra | awk '{ print $2 }' | sed 's/penumbra//;s/,//')
-echo "Current balance: $balance"
+#echo "Current balance: $balance"
 
 # Calculate the amount to delegate
 int_balance=${balance%%.*}
 amount=$(($int_balance-5))
-echo "Amount to delegate: $amount"
+#echo "Amount to delegate: $amount"
 
 # Get the first validator from the list of active validators
 validator=$(pcli query validator list | grep penumbra | awk '{ gsub(/^[[:space:]]+|[[:space:]]+$/, ""); print $6 }' | head -1)
-echo "The address of the active validator: $validator"
+#echo "The address of the active validator: $validator"
 
 # Delegate the tokens to the first active validator
 echo "Delegating $amount penumbra tokens to $validator"
-#pcli tx delegate "${amount}penumbra" --to "$validator"
+pcli tx delegate "${amount}penumbra" --to "$validator"
