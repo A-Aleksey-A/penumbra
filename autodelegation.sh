3a001fbb8aefee83ee40d9f03d3ebe756d035bf3
A-Aleksey-A <126988346+A-Aleksey-A@users.noreply.github.com>
2023-03-14 22:13:09 +0200

    Update autodelegation.sh

--- autodelegation.sh
+++ autodelegation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-validator=$(pcli query validator list | grep penumbra | awk '{ print $6 }' | sed 's/\x1b\[[0-9;]*[mG]//g' | head -1)
-echo $validator > penumbra-validators.txt
+validator=$(pcli query validator list | grep penumbra | awk '{ print $6 }' | head -1 | sed 's/\x1b\[[0-9;]*[mG]//g') > penumbra-validators.txt
+#echo $validator > penumbra-validators.txt
 
 #pcli query validator list > penumbra-validators.txt
