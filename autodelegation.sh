babdf9fd4550d4e0c24e6050d3c8e90039373b49
A-Aleksey-A <126988346+A-Aleksey-A@users.noreply.github.com>
2023-03-14 22:22:37 +0200

    Update autodelegation.sh

--- autodelegation.sh
+++ autodelegation.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
 #validator=$(pcli query validator list | grep penumbra | awk '{ print $6 }' | head -1)
-validator=$(pcli query validator list | grep penumbra | awk '{ print $6 }' | head -1 | sed 's/\x1B\[[0-9;]*[mK]//g')
-echo $validator > penumbra-validators.txt
+#validator=$(pcli query validator list | grep penumbra | awk '{ print $6 }' | head -1 | sed 's/\x1B\[[0-9;]*[mK]//g')
+#echo $validator > penumbra-validators.txt
 
 #pcli query validator list > penumbra-validators.txt
+
+
+#!/bin/bash
+# Выводит первого активного валидатора из списка
+validator=$(pcli query validator list | grep penumbra | awk '{ print $6 }' | head -1 | sed 's/\x1B\[[0-9;]*[mK]//g')
+echo $validator
+
+# Выводит баланс для стейкинга (пока минус 50)
+current_balance=$(pcli view balance | grep penumbra | awk '{ print $1 }' | tr -d 'penumbra' | head -1)
+dif=10
+stake_balance=$(($current_balance-$dif))
+echo $current_balance
+echo $stake_balance
+
+# Стейкает монетки первому активному валидатору из списка
+#stake=$(pcli tx delegate "${stake_balance}"penumbra --to $validator)
+#echo $stake
