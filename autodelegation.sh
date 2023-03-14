1fe6a2af1c78cafd60257ca1d5858ba5c56c5836
A-Aleksey-A <126988346+A-Aleksey-A@users.noreply.github.com>
2023-03-14 23:04:45 +0200

    Update autodelegation.sh

--- autodelegation.sh
+++ autodelegation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "-----------------------------------------------------------------------------"
-version=$(pcli -V | awk '{ print $2 }') &>/dev/null
 echo "Проверяем текущую версию..."
+version=$(pcli -V | awk '{ print $2 }') &>/dev/null
 sleep 15
 echo "-----------------------------------------------------------------------------"
-current_balance=$(pcli view balance | grep penumbra | awk '{ print $2 }' | tr -d 'penumbra' | head -1) &>/dev/null
 echo "Проверяем баланс..."
+current_balance=$(pcli view balance | grep penumbra | awk '{ print $2 }' | tr -d 'penumbra' | head -1) &>/dev/null
 sleep 15
 echo "-----------------------------------------------------------------------------"
 if [ $current_balance -le 10 ]; then
@@ -15,15 +15,16 @@ elif [ $current_balance -gt 10 ]; then
   echo "Подготавливаем токены к делегированию..."
   dif=10
   delegate_balance=$(($current_balance-$dif))
-  validator=$(pcli query validator list | grep penumbra | awk '{ print $6 }' | head -1 | sed 's/\x1B\[[0-9;]*[mK]//g') &>/dev/null
   sleep 15
   echo "-----------------------------------------------------------------------------"
   echo "Ищем подходящего валидатора..."
-#  delegate=$(pcli tx delegate "${delegate_balance}"penumbra --to $validator) &>/dev/null
+  validator=$(pcli query validator list | grep penumbra | awk '{ print $6 }' | head -1 | sed 's/\x1B\[[0-9;]*[mK]//g') &>/dev/null
   sleep 15
   echo "-----------------------------------------------------------------------------"
   echo "Делегируем токены валидатору..."
-#  echo $delegate
+#  delegate=$(pcli tx delegate "${delegate_balance}"penumbra --to $validator) &>/dev/null
+  sleep 15
+#  echo $delegate  
   echo "-----------------------------------------------------------------------------"
   echo "Обновление и делегация выполнены:"
   echo "Текущая версия: $version"
